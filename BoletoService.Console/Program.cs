94e768960d6b7206f4dd1380646d9a9e06afeaf4
Vinicius Oliveira <eovinicius10@gmail.com>
2025-03-21 15:34:28 -0300

    wip

--- BoletoService.Console/Program.cs
+++ BoletoService.Console/Program.cs
@@ -1,10 +1,47 @@
 ﻿using BoletoService.Console;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
-var valorTotal = 24119.00m;
-var codigoCliente = "1511116008";
-var numeroFatura = "400479216229";
+var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production";
 
-var boleto = BuilderBoleto.Execute(valorTotal, codigoCliente, numeroFatura);
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .Build();
 
-Console.WriteLine(boleto.CodigoDeBarras);
-Console.WriteLine(boleto.LinhaDigitavel);
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+var serviceCollection = new ServiceCollection();
+try
+{
+    Log.Information("Aplicação iniciada.");
+    Log.Information("Ambiente carregado: {Environment}", environment);
+
+    var serviceProvider = serviceCollection.BuildServiceProvider();
+
+    var builderBoleto = serviceProvider.GetService<BuilderBoleto>()!;
+    // Arrange
+    var valorTotal = 55.93m;
+    var codigoCliente = "1538519032";
+    var numeroFatura = "402191846138";
+
+    // Act
+    var builderBolet = new BuilderBoleto();
+    var boleto = builderBolet.Execute(valorTotal, codigoCliente, numeroFatura);
+
+    var x = builderBoleto.Execute(valorTotal, codigoCliente, numeroFatura);
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Ocorreu um erro inesperado.");
+}
+finally
+{
+    Log.Information("Aplicação finalizada.");
+    Log.CloseAndFlush();
+}
--- BoletoService.Test/BuilderBoletoTests.cs
+++ BoletoService.Test/BuilderBoletoTests.cs
@@ -5,7 +5,7 @@ namespace BoletoService.Test;
 public class BuilderBoletoTests
 {
     [Fact]
-    public void CalculaDigitosVerificadores_DeveRetornarValoresCorretos()
+    public void CalculaDigitosVerificadoresDeveRetornarValoresCorretos()
     {
         // Arrange
         var valorTotal = 24119.00m;
@@ -21,5 +21,24 @@ public class BuilderBoletoTests
         Assert.NotNull(boleto.LinhaDigitavel);
         Assert.Equal("84800000241190003791511116008040047921622988", boleto.CodigoDeBarras);
         Assert.Equal("84800000241-6 19000379151-5 11160080400-5 47921622988-0", boleto.LinhaDigitavel);
+    }
+
+    [Fact]
+    public void CalculaDigitosVerificadoresDeveRetornarValoresCorretos2()
+    {
+        // Arrange
+        var valorTotal = 55.93m;
+        var codigoCliente = "1538519032";
+        var numeroFatura = "402191846138";
+
+        // Act
+        var builderBoleto = new BuilderBoleto();
+        var boleto = builderBoleto.Execute(valorTotal, codigoCliente, numeroFatura);
+
+        // Assert
+        Assert.NotNull(boleto.CodigoDeBarras);
+        Assert.NotNull(boleto.LinhaDigitavel);
+        Assert.Equal("84850000000559303791538519032040219184613896", boleto.CodigoDeBarras);
+        Assert.Equal("84850000000-5 55930379153-7 85190320402-4 19184613896-4", boleto.LinhaDigitavel);
     }
 }
