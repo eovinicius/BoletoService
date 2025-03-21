82897b5d9ffc4ce8d161da59303fef0a35636058
Vinicius Oliveira <eovinicius10@gmail.com>
2025-03-21 11:28:10 -0300

    wip

---
+++ BoletoService.Console/BuilderCodigoBarra.cs
@@ -0,0 +1,66 @@
+namespace BoletoService.Console;
+
+public static class BuilderCodigoBarra
+{
+    private const string MODULO_11 = "8480";
+    private const string CODIGO_CONVENIO = "0379";
+    private const string CANAL_EMISSOR = "88";
+
+    public static (string, string) Execute(decimal valorTotal, string codigoCliente, string numeroFatura)
+    {
+        const int TAMANHO_VALOR_TOTAL = 11;
+        const int TAMANHO_CODIGO_CLIENTE = 10;
+        const int TAMANHO_NUMERO_FATURA = 13;
+
+        string codigoBarras = string.Concat(
+            MODULO_11,
+            valorTotal.ToString().Replace(",", "").PadLeft(TAMANHO_VALOR_TOTAL, '0'),
+            CODIGO_CONVENIO,
+            codigoCliente.PadLeft(TAMANHO_CODIGO_CLIENTE, '0'),
+            numeroFatura.PadLeft(TAMANHO_NUMERO_FATURA, '0'),
+            CANAL_EMISSOR);
+
+        return CalculaDigitosVerificadores(codigoBarras);
+    }
+
+    private static (string valorCalculado, string linhaDigitavel) CalculaDigitosVerificadores(string valor)
+    {
+        int dvGeral = CalcularModulo11(valor, MultiplicadoresModulo11.Geral);
+        valor = string.Concat(valor.Substring(0, 3), dvGeral, valor.Substring(4));
+
+        int dvBloco1 = CalcularModulo11(valor, MultiplicadoresModulo11.Bloco1);
+        int dvBloco2 = CalcularModulo11(valor, MultiplicadoresModulo11.Bloco2);
+        int dvBloco3 = CalcularModulo11(valor, MultiplicadoresModulo11.Bloco3);
+        int dvBloco4 = CalcularModulo11(valor, MultiplicadoresModulo11.Bloco4);
+
+        string linhaDigitavel = FormatLinhaDigitavel(valor, dvBloco1, dvBloco2, dvBloco3, dvBloco4);
+
+        return (valor, linhaDigitavel);
+    }
+
+    private static int CalcularModulo11(string valor, Dictionary<int, int> multiplicadores)
+    {
+        int soma = 0;
+        for (int i = 0; i < valor.Length; i++)
+        {
+            if (multiplicadores.TryGetValue(i + 1, out int peso))
+            {
+                soma += (valor[i] - '0') * peso;
+            }
+        }
+
+        int resto = soma % 11;
+        return (resto == 0 || resto == 1) ? 0 : (resto == 10 ? 1 : 11 - resto);
+    }
+
+    private static string FormatLinhaDigitavel(string valor, int dv1, int dv2, int dv3, int dv4)
+    {
+        return string.Format(
+            "{0}-{1} {2}-{3} {4}-{5} {6}-{7}",
+            valor.Substring(0, 11), dv1,
+            valor.Substring(11, 11), dv2,
+            valor.Substring(22, 11), dv3,
+            valor.Substring(33, 11), dv4
+        );
+    }
+}
---
+++ BoletoService.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿using BoletoService.Console;
+
+var valorTotal = 24119.00m;
+var codigoCliente = "1511116008";
+var numeroFatura = "400479216229";
+
+(string x, string y) = BuilderCodigoBarra.Execute(valorTotal, codigoCliente, numeroFatura);
+Console.WriteLine(x);
+Console.WriteLine(y);
