999b6151225a8822fddcfc498cca8fb16d59c5bc
Vinicius Oliveira <eovinicius10@gmail.com>
2025-03-21 12:08:32 -0300

    feat: Rename linhaDigitavel to LinhaDigitavel

--- BoletoService.Console/Boleto.cs
+++ BoletoService.Console/Boleto.cs
@@ -3,10 +3,10 @@ namespace BoletoService.Console;
 public class Boleto
 {
     public string CodigoDeBarras { get; private set; }
-    public string linhaDigitavel { get; private set; }
+    public string LinhaDigitavel { get; private set; }
     public Boleto(string codigoDeBarras, string linhaDigitavel)
     {
         CodigoDeBarras = codigoDeBarras;
-        this.linhaDigitavel = linhaDigitavel;
+        LinhaDigitavel = linhaDigitavel;
     }
 }
